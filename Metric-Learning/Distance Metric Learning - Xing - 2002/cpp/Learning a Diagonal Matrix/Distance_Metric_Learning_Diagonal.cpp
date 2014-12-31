d5711dff9c560626ccdfca4935ac5f3e34af5543
zck921031 <zck921031@qq.com>
2014-12-31 17:35:32 +0800

    梯度下降
    如果要收敛到全局最优，代价函数必须是凸的。使用论文的代价函数，简单的梯度下降收敛到了局部最优。

--- Metric-Learning/Distance Metric Learning - Xing - 2002/cpp/Learning a Diagonal Matrix/Distance_Metric_Learning_Diagonal.cpp
+++ Metric-Learning/Distance Metric Learning - Xing - 2002/cpp/Learning a Diagonal Matrix/Distance_Metric_Learning_Diagonal.cpp
@@ -5,7 +5,7 @@ const char *train_file_dir = "../../../../dataset/wine/wine_train.data";
 const char *test_file_dir = "../../../../dataset/wine/wine_train.test";
 vector<int> train_lable, test_lable;
 vector< vector<double> > train_data, test_data;
-int N, D=13;
+int N, dim=13;
 /**
 wine.names describes the format of wine.data
 */
@@ -13,8 +13,8 @@ bool read_wine(FILE* fin,vector<int>&lable, vector< vector<double> >&data)
 {
     int tmplable;
     if ( fscanf(fin, "%d", &tmplable) != 1 ) return true;
-    vector<double> tmpdata(D);
-    for (int i=0; i<D; i++){
+    vector<double> tmpdata(dim);
+    for (int i=0; i<dim; i++){
         if ( fscanf(fin, ",%lf", &tmpdata[i] ) != 1 ) return false;
     }
     lable.push_back( tmplable );
@@ -24,7 +24,7 @@ bool read_wine(FILE* fin,vector<int>&lable, vector< vector<double> >&data)
 void output_data(){
     for (int i=0; i<N; i++){
         printf("%d", train_lable[i]);
-        for (int j=0; j<D; j++){
+        for (int j=0; j<dim; j++){
             printf(",%.3f", train_data[i][j]);
         }
         puts("");
@@ -109,16 +109,14 @@ public:
             }
         }
 
-        auto gg = gradient(A);
-        for (int i=0; i<d; i++){
-            printf("%.7f ", gg[i]);
-        }
-        exit(0);
-
-
+//        auto gg = gradient(A);
+//        for (int i=0; i<d; i++){
+//            printf("%.7f ", gg[i]);
+//        }
+//        exit(0);
         double lambda = 1;
         int iter = 0;
-        while(iter<100){
+        while(lambda>eps*eps){
             ++iter;
             auto grad = gradient(A);
             vector<double> tmpA(A);
@@ -128,19 +126,27 @@ public:
                 tmpA[i] -= lambda*grad[i];
                 if ( tmpA[i]<0 ) flag = false;
             }
-//            if (flag) {
-//                    printf("%.6f %.6f\n", g(A), g(tmpA) );
+
+            if (flag) {
+                printf("pre: %.6f  new: %.6f  iter:%d\n", g(A), g(tmpA), iter );
 //                char str[200];
+//                puts("a:");
 //                for (int i=0; i<13; i++){
-//                    printf("%.4f ", tmpA[i]);
+//                    printf("%f ", tmpA[i]);
 //                }
 //                puts("");
+//                puts("gradient:");
 //                for (int i=0; i<13; i++){
-//                    printf("%.4f ", grad[i]*lambda);
+//                    printf("%f ", grad[i]);
 //                }
 //                puts("");
-//                gets(str);
-//            }
+//                puts("fd_1st_D");
+//                for (int i=0; i<13; i++){
+//                    printf("%f ", -(grad[i]-S_sqrsum[i]) );
+//                }
+//                puts("");
+                //gets(str);
+            }
 
             if ( g(tmpA)<g(A) && flag ){
                 A = tmpA;
@@ -148,7 +154,7 @@ public:
             }else{
                 lambda *= 0.5;
             }
-            printf("iterator %d, lambda=%.15f score:%.6f\n", iter, lambda, g(A) );
+            //printf("iterator %d, lambda=%.15f score:%.6f\n", iter, lambda, g(A) );
 //            for (int i=0; i<d; i++) printf("%.4f ", grad[i]);
 //            puts("");
             //break;
@@ -163,9 +169,15 @@ int main(){
     while( !feof(ftrain) ){
         read_wine(ftrain, train_lable, train_data);
     }
-    N = train_lable.size();
 
+    train_lable.clear();
+    train_data.clear();
+    train_lable = {1,1,1,1,2,2,2,2};
+    train_data = { {2,20},{3,30},{4,40},{3,40},
+    {5,50},{6,60},{7,70},{6,50} };
 
+    N = train_data.size();
+    dim = train_data[0].size();
     vector< pair<int,int> > S,D;
     for (int i=0; i<N; i++){
         for (int j=i+1; j<N; j++){
@@ -181,28 +193,51 @@ int main(){
     auto A = test.solve(train_data, S, D, 1);
 
 
-    for (int i=0; i<13; i++){
-        printf("%.6f\n", A[i]);
+
+    for (int i=0; i<dim; i++){
+        printf("%.12f\n", A[i]);
     }
 
-    A = {  17.1348,
-         0,
-         0,
-         0,
-         0,
-         0,
-   78.7429,
-         0,
-         0,
-    4.9630,
-         0,
-   46.8067,
-    0.0002};
-    printf("score: %.9f\n", test.g(A) );
+//    A = {  17.1348,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//   78.7429,
+//         0,
+//         0,
+//    4.9630,
+//         0,
+//   46.8067,
+//    0.0002};
+//    A = {
+//   9.393331359993496,
+//                   0,
+//                   0,
+//   0.175414620486221,
+//                   0,
+//                   0,
+//  20.149933035014229,
+//                   0,
+//                   0,
+//   2.655630120871724,
+//                   0,
+//  11.704827180701225,
+//   0.000109877997622
+//    };
+//    A = {51.943762417332515,0};
+//    printf("score: %.9f\n", test.g(A) );
 
-   for (int i=0; i<13; i++){
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++){
+            printf("%.6f ",test.distance2(train_data[i], train_data[j], A) );
+        }
+        puts("");
+    }
+    for (int i=0; i<dim; i++){
         printf("%.6f\n", A[i]);
-   }
+    }
     int u,v;
     while( scanf("%d%d", &u, &v)!=EOF ){
         printf("%.6f\n", test.distance2(train_data[u], train_data[v], A) );
